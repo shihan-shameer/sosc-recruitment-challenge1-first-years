be7fca0f5f5a6d88fa36a02e16ec959c477fb431
shihan-shameer <shihanshameer9898@gmail.com>
2025-12-07 12:35:53 +0530

    Update script.js fixed button ID and enhanced interactivity

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,20 @@
-document.getElementById("info-bnt").addEventListener("click", () => {
+document.getElementById("info-btn").addEventListener("click", () => {
     alert("JS effect working!");
+});
+
+// create floating particles
+for (let i = 0; i < 40; i++) {
+    let dot = document.createElement("span");
+    dot.style.left = Math.random() * window.innerWidth + "px";
+    dot.style.animationDuration = 3 + Math.random() * 5 + "s";
+    dot.style.opacity = Math.random();
+    document.body.appendChild(dot);
+}
+// Hover animation on name
+const name = document.getElementById("name");
+name.addEventListener("mouseover", () => {
+    name.style.color = "#00ffcc";
+});
+name.addEventListener("mouseout", () => {
+    name.style.color = "white";
 });
